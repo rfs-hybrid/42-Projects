7c47418f0f078174bd44ed4667fd0036be141559
maaugust <maaugust@student.42porto.com>
2025-04-10 14:54:28 +0100

    Added substr and strjoin functions

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,8 +6,24 @@
 /*   By: maaugust <maaugust@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 13:33:14 by maaugust          #+#    #+#             */
-/*   Updated: 2025/04/08 13:33:26 by maaugust         ###   ########.fr       */
+/*   Updated: 2025/04/10 14:52:48 by maaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = (char *)ft_calloc(s1_len + s2_len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	ft_memcpy(str, s1, s1_len);
+	ft_memcpy(str + s1_len, s2, s2_len);
+	return (str);
+}
--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,8 +6,29 @@
 /*   By: maaugust <maaugust@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 13:32:39 by maaugust          #+#    #+#             */
-/*   Updated: 2025/04/08 13:32:45 by maaugust         ###   ########.fr       */
+/*   Updated: 2025/04/10 14:40:00 by maaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	if (start < ft_strlen(s))
+	{
+		while (*(s + (start + i)) && i < len)
+		{
+			*(sub + i) = *(s + (start + i));
+			i++;
+		}
+	}
+	*(sub + i) = '\0';
+	return (sub);
+}
