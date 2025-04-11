a198264ab35b4ed594e14ce3718681c391ada8c0
maaugust <maaugust@student.42porto.com>
2025-04-11 16:10:57 +0100

    Fixed wrong memory allocation in substr function

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,23 +6,38 @@
 /*   By: maaugust <maaugust@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 13:32:39 by maaugust          #+#    #+#             */
-/*   Updated: 2025/04/10 16:30:34 by maaugust         ###   ########.fr       */
+/*   Updated: 2025/04/11 16:10:31 by maaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static char	*allocate_memory(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+
+	if (start >= ft_strlen(s))
+		str = (char *)ft_calloc(1, sizeof(char));
+	else if (ft_strlen(s) - start < len)
+		str = (char *)ft_calloc(ft_strlen(s) - start + 1, sizeof(char));
+	else
+		str = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	return (str);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	i;
 
-	sub = (char *)ft_calloc(len + 1, sizeof(char));
+	sub = allocate_memory(s, start, len);
 	if (!sub)
 		return (NULL);
-	i = 0;
 	if (start < ft_strlen(s))
 	{
+		i = 0;
 		while (*(s + (start + i)) && i < len)
 		{
 			*(sub + i) = *(s + (start + i));
