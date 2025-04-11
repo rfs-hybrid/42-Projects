9de3f882e394e0562f7e7bc848738d032b27c0cb
maaugust <maaugust@student.42porto.com>
2025-04-11 18:03:18 +0100

    Fixed memory deallocation issues and performed optimization in split function

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: maaugust <maaugust@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 13:33:49 by maaugust          #+#    #+#             */
-/*   Updated: 2025/04/11 15:12:40 by maaugust         ###   ########.fr       */
+/*   Updated: 2025/04/11 18:01:59 by maaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,25 +33,19 @@ static size_t	count_words(const char *s, char c)
 	return (n_words);
 }
 
-static size_t	word_length(const char *s, char c)
+static char	*separate_string(const char *s, char c)
 {
+	char	*word;
 	size_t	len;
+	size_t	i;
 
 	len = 0;
 	while (*(s + len) && *(s + len) != c)
 		len++;
-	return (len);
-}
-
-static char	*separate_string(const char *s, char c)
-{
-	char	*word;
-	size_t	i;
-
-	i = 0;
-	word = (char *) ft_calloc(word_length(s, c) + 1, sizeof(char));
+	word = (char *) ft_calloc(len + 1, sizeof(char));
 	if (!word)
 		return (NULL);
+	i = 0;
 	while (*(s + i) && *(s + i) != c)
 	{
 		*(word + i) = *(s + i);
@@ -60,26 +54,38 @@ static char	*separate_string(const char *s, char c)
 	return (word);
 }
 
+static char	**free_memory(char **split, size_t n)
+{
+	if (n)
+	{
+		while (n-- > 0)
+			free(split[n]);
+	}
+	free(split);
+	return (NULL);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**split;
-	size_t	n_words;
 	size_t	i;
 	size_t	j;
 
-	n_words = count_words(s, c);
-	split = (char **)ft_calloc(n_words + 1, sizeof(char *));
+	split = (char **)ft_calloc(count_words(s, c) + 1, sizeof(char *));
 	if (!split || !s)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (*(s + i) && j < n_words)
+	while (*(s + i) && j < count_words(s, c))
 	{
 		while (*(s + i) && *(s + i) == c)
 			i++;
 		if (*(s + i) && *(s + i) != c)
 		{
-			*(split + j++) = separate_string(s + i, c);
+			*(split + j) = separate_string(s + i, c);
+			if (!(*(split + j)))
+				return (free_memory(split, j));
+			j++;
 			while (*(s + i) && *(s + i) != c)
 				i++;
 		}
