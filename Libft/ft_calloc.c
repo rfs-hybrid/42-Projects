319f642ee6db4406142e7c24fda8689ef5dcebe2
maaugust <maaugust@student.42porto.com>
2025-04-17 19:07:37 +0100

    Added condition in calloc function in case of size overflow

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: maaugust <maaugust@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 13:32:04 by maaugust          #+#    #+#             */
-/*   Updated: 2025/04/10 14:13:45 by maaugust         ###   ########.fr       */
+/*   Updated: 2025/04/17 19:06:46 by maaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned char	*arr;
 
+	if (size && nmemb > SIZE_MAX / size)
+		return (NULL);
 	arr = (unsigned char *)malloc(sizeof(unsigned char) * (nmemb * size));
 	if (!arr)
 		return (NULL);
