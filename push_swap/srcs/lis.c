a3f386b3080031a1f032cdde4e868dd75b94c846
maaugust <maaugust@student.42porto.com>
2025-06-07 15:49:17 +0100

    Added new lis.c file to push_swap project

--- push_swap/srcs/lis.c
+++ push_swap/srcs/lis.c
@@ -6,13 +6,21 @@
 /*   By: maaugust <maaugust@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 15:26:32 by maaugust          #+#    #+#             */
-/*   Updated: 2025/06/07 15:29:28 by maaugust         ###   ########.fr       */
+/*   Updated: 2025/06/07 15:47:26 by maaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int *ft_lis_array_size(t_stack *a)
+t_stack *get_lis(t_stack *s, int max)
 {
+    t_stack *lis;
+    t_stack *tmp;
+    int     pos;
 
+    pos = 0;
+    lis = s;
+
+
+    return (lis);
 }
