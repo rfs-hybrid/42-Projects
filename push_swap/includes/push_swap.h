44b1c68e4af982fd85ce678fb1ec49a3c07d3f84
maaugust <maaugust@student.42porto.com>
2025-06-19 16:45:50 +0100

    Updated push_swap header file

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: maaugust <maaugust@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 16:40:47 by maaugust          #+#    #+#             */
-/*   Updated: 2025/06/18 16:11:11 by maaugust         ###   ########.fr       */
+/*   Updated: 2025/06/19 15:48:43 by maaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ void	ft_free_words(char **words);
 long	ft_atol(const char *nptr);
 int		ft_strcmp(const char *s1, const char *s2);
 
-void	ft_print_error(void);\
+void	ft_print_error(void);
 void	ft_stackadd_back(t_stack **stack, t_stack *new);
 void	ft_stackclear(t_stack **stack);
 int		ft_stack_size(t_stack *stack);
@@ -68,7 +68,7 @@ int		*ft_lis_sequence(int *arr, int size, int *lis_len);
 void	ft_run_algorithm(t_stack **a, t_stack **b, t_lis lis);
 void	ft_stack_lis_sort(t_stack **a, t_stack **b, t_lis lis);
 t_stack	*find_cheapest(t_stack **stack);
-void	run_commands(t_stack **s1, t_stack **s2, t_stack *node, bool is_a);
+void	run_commands(t_stack **s1, t_stack **s2, t_stack *node);
 void	stack_index_cost(t_stack **stack);
 
 #endif
